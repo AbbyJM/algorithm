768b4af1871bab4ab03b7ecf38eeed118cd3fa76
Zhang Jianming <1358890401@qq.com>
2018-05-28 20:59:28 +0800

    插入排序算法实现

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,1 +1,49 @@
+#include<iostream>
+#include<time.h>
+using namespace std;
+//the insertion sort algorithm
 
+
+/*
+ * param a ,the array to be sorted
+ * param n ,the size of the array
+ */
+void insertion_sort(int* a,int n)
+{
+	for (int i = 1; i <= n-1; i++)
+	{
+		int key = a[i];
+		int j = i-1;
+		while (j >= 0 && a[j] > key) 
+		{
+			a[j + 1] = a[j];
+			j--;
+		}
+		a[j + 1] = key;
+	}
+}
+
+void insertion_sort_exam()
+{
+	int a[10000];
+	srand((unsigned int)time(NULL));
+
+	for (int i = 0; i < 10000; i++)
+	{
+		a[i] = rand() % 10000;
+	}
+
+
+	clock_t start = clock();
+
+	insertion_sort(a, sizeof(a) / sizeof(int));
+
+	clock_t end = clock();
+	cout << "the sorted array: " << endl;
+	for (int i = 0; i < 10000; i++)
+	{
+		cout << a[i] << endl;
+	}
+
+	cout << "total time used of insertion sort is " << end-start <<" ms "<<endl;
+}
